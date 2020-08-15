95a8cad66afb0d9044f75e58665594b4a21a97b9
Elaine Oglesbay <Elaine@Family-Macbook-Pro.local>
2020-08-15 11:53:53 -0400

    removing unneeded boilerplate code and setting up reducer for posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
+        <h1>Hello!</h1>
       </header>
     </div>
   );
---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = []
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState: initialState,
+    reducers: {
+
+    }
+})
+
+export default postsSlice.reducer
