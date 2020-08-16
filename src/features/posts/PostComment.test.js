33782f3d4dec36d4d707985d8753a2dae6965e32
Elaine Oglesbay <Elaine@Family-Macbook-Pro.local>
2020-08-16 16:37:41 -0400

    fixing failing tests related to conditional rendering

--- src/features/posts/PostComment.test.js
+++ src/features/posts/PostComment.test.js
@@ -15,28 +15,77 @@ const comment = {
     },
     date: sub(new Date(), { minutes: 20 }).toISOString(),
     content: 'This is a comment.',
-    likes: 0
+    likes: 0,
+    showCommentForm: false
 }
-let wrapper
-
-beforeEach(() => {
-    wrapper = shallow(<PostComment postId={postId} comment={comment}/>)
-}) 
     
 describe('PostComment', () => {
     it ('renders the correct info for user making comment', () => {
+        const comment = { 
+            id: '111',
+            user: {
+                name: 'Mary',
+                location: 'Florida'
+            },
+            date: sub(new Date(), { minutes: 20 }).toISOString(),
+            content: 'This is a comment.',
+            likes: 0,
+            showCommentForm: false
+        }
+        let wrapper = shallow(<PostComment postId={postId} comment={comment}/>)
         expect(wrapper.find('[data-testid="user-name"]').text()).toEqual(comment.user.name)
         expect(wrapper.find('[data-testid="user-location"]').text()).toEqual(comment.user.location)
         expect(wrapper.find('[data-testid="user-comment"]').text()).toEqual(comment.content)
     })
 
     it('passes the correct props to custom components', () => {
+        const comment = { 
+            id: '111',
+            user: {
+                name: 'Mary',
+                location: 'Florida'
+            },
+            date: sub(new Date(), { minutes: 20 }).toISOString(),
+            content: 'This is a comment.',
+            likes: 0,
+            showCommentForm: false
+        }
+        let wrapper = shallow(<PostComment postId={postId} comment={comment}/>)
         expect(wrapper.find('[data-testid="time-passed"]').prop('timestamp')).toEqual(comment.date)
         expect(wrapper.find('[data-testid="comment-reactions"]').prop('postId')).toEqual(postId)
         expect(wrapper.find('[data-testid="comment-reactions"]').prop('comment')).toEqual(comment)
-        expect(wrapper.find('[data-testid="edit-comment-form"]').prop('postId')).toEqual(postId)
-        expect(wrapper.find('[data-testid="edit-comment-form"]').prop('comment')).toEqual(comment)
+    })
 
+    it('does not show comment form by default', () => {
+        const comment = { 
+            id: '111',
+            user: {
+                name: 'Mary',
+                location: 'Florida'
+            },
+            date: sub(new Date(), { minutes: 20 }).toISOString(),
+            content: 'This is a comment.',
+            likes: 0,
+            showCommentForm: false
+        }
+        let wrapper = shallow(<PostComment postId={postId} comment={comment}/>)
+        expect(wrapper.find('[data-testid="edit-comment-form"]')).toHaveLength(0)
+    })
 
+    it('shows comment form and hides comment text when state is updated to true', () => {
+        const comment = { 
+            id: '111',
+            user: {
+                name: 'Mary',
+                location: 'Florida'
+            },
+            date: sub(new Date(), { minutes: 20 }).toISOString(),
+            content: 'This is a comment.',
+            likes: 0,
+            showCommentForm: true
+        }
+        let wrapper = shallow(<PostComment postId={postId} comment={comment}/>)
+        expect(wrapper.find('[data-testid="edit-comment-form"]')).toHaveLength(1)
+        expect(wrapper.find('[data-testid="user-comment"]')).toHaveLength(0)
     })
 })
